470a1e16321908a93f80608f1329e2c186f5c5b7
Patrick Hudson <patrick@hudson.bz>
2014-10-18 04:57:18 -0400

    Scripted auto-commit on change (2014-10-18 04:57:18) by gitwatch.sh

--- scripts/commit.tcl
+++ scripts/commit.tcl
@@ -1,10 +1,10 @@
 #Looks up random insults
 bind pub - !commit
 proc commit {nick host hand chan arg} {
-
   package require http
   set url "http://whatthecommit.com/"
   set page [http::data [http::geturl $url]]
+  regsub -all {(?:\n|\t|\v|\r|\x01)} $page " " page
   if {[regexp -nocase {<div id="content">(.*?)</div>} $page commit]} {
   	regsub -nocase -- {<p>(.*?)</p>} $commit "\\1" commit
   	putserv "PRIVMSG $chan :$commit"
