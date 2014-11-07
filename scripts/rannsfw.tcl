7299e2b3369983aded23bc99854c79062d923b27
Patrick Hudson <patrick@hudson.bz>
2014-11-07 06:38:00 -0500

    Scripted auto-commit on change (2014-11-07 06:38:00) by gitwatch.sh

--- scripts/rannsfw.tcl
+++ scripts/rannsfw.tcl
@@ -17,6 +17,8 @@ proc getTrigger {} {
   return $pubtrig
 }
 bind pub - ${pubtrig}tits tits:pub
+bind pub - ${pubtrig}ass ass:pub
+bind pub - ${pubtrig}pussy pussy:pub
 
 proc tits:pub {nick host hand chan arg} {
   set page [myRand 0 50]
@@ -40,7 +42,53 @@ proc tits:pub {nick host hand chan arg} {
   }
   putserv "PRIVMSG $chan :Your random tits! $link - Title: $title"
   http::cleanup $token
-}  
+}
+proc ass:pub {nick host hand chan arg} {
+  set page [myRand 0 50]
+  set theurl "https://api.imgur.com/3/gallery/r/ass/time/$page"
+  dict set hdr Authorization "Client-ID cefb2e6ae32f74f"
+  http::register https 443 [list ::tls::socket -tls1 1]
+  set token [http::geturl $theurl -headers $hdr -query]
+  set responseBody [::json::json2dict [http::data $token]]
+  set data [lindex $responseBody 1]
+  set linkid [myRand 0 30]
+  set imagedata [lindex $data $linkid]
+  if {[regexp -nocase {link (.*?) reddit_comments} $imagedata " " link]} {
+    regsub -nocase -- {link (.*?) reddit_comments} $link "\\1" link
+  } else {
+    set link "Wohhh there cowboy, slow down!"
+  }
+  if {[regexp -nocase {title {(.*?)} description} $imagedata " " title]} {
+    regsub -nocase -- {title {(.*?)} description} $title "\\1" title
+  } else {
+    set title "Title Unknown"
+  }
+  putserv "PRIVMSG $chan :Your random ass! $link - Title: $title"
+  http::cleanup $token
+}
+proc pussy:pub {nick host hand chan arg} {
+  set page [myRand 0 50]
+  set theurl "https://api.imgur.com/3/gallery/r/pussy/time/$page"
+  dict set hdr Authorization "Client-ID cefb2e6ae32f74f"
+  http::register https 443 [list ::tls::socket -tls1 1]
+  set token [http::geturl $theurl -headers $hdr -query]
+  set responseBody [::json::json2dict [http::data $token]]
+  set data [lindex $responseBody 1]
+  set linkid [myRand 0 30]
+  set imagedata [lindex $data $linkid]
+  if {[regexp -nocase {link (.*?) reddit_comments} $imagedata " " link]} {
+    regsub -nocase -- {link (.*?) reddit_comments} $link "\\1" link
+  } else {
+    set link "Wohhh there cowboy, slow down!"
+  }
+  if {[regexp -nocase {title {(.*?)} description} $imagedata " " title]} {
+    regsub -nocase -- {title {(.*?)} description} $title "\\1" title
+  } else {
+    set title "Title Unknown"
+  }
+  putserv "PRIVMSG $chan :Your random pussy! $link - Title: $title"
+  http::cleanup $token
+}
 proc myRand { min max } {
     set maxFactor [expr [expr $max + 1] - $min]
     set value [expr int([expr rand() * 100])]
